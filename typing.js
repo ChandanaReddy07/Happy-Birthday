0d6f556b6c543d5d57e8eb217cbf2ad6a9b72b8d
dcr7978933k <b219021@iiit-bh.ac.in>
2021-08-01 06:03:59 -0700

    first commit

---
+++ typing.js
@@ -0,0 +1,39 @@
+const words = ["Every time I do something right,...", "I remember that it was you...", "There are no words with which I can tell u...","thanku..mom..","and so ur the best MOM in the whole world...!!","my Omni...","Happy Birthday Amma...!!!"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 150);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 80);
+	};
+	loopDeleting();
+};
+
+typingEffect();
